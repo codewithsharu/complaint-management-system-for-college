aa35652e0930da320e5a66f992bcc194b9aea44a
Shareen <codewithsharu@gmail.com>
2024-07-15 11:36:52 +0530

    1136

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -28,4 +28,15 @@ router.get('/success' , userController.successGoogleLogin);
 // failure 
 router.get('/failure' , userController.failureGoogleLogin);
 
+router.get('/lo', (req, res) => {
+    req.logout(); // Passport.js function to clear login session
+    req.session.destroy(err => {
+        if (err) {
+            console.error('Error destroying session:', err);
+            return res.status(500).send('Error logging out');
+        }
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
